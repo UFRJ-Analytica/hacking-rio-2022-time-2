7179c07df803d8fab4f8cd1815705f0c60956783
KeithFabre <keithfmacedo@poli.ufrj.br>
2022-09-03 02:49:26 -0300

    template paginas

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
-  <v-app dark>
+  <v-app class="font">
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
       fixed
       app
+      id="sidebar"
     >
+
+    <h1 id="hurb-title">Hurb</h1>
+
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -15,6 +19,7 @@
           router
           exact
         >
+
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -23,68 +28,32 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+
     </v-navigation-drawer>
+
+
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      id="header"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon color="black">mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
     </v-app-bar>
-    <v-main>
+
+
+    <v-main id="main">
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+      
   </v-app>
 </template>
 
@@ -96,23 +65,90 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
+
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'Início',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Resumo',
+          to: '/resumo'
+        },
+        {
+          icon: 'mdi-chart-bar',
+          title: 'Lazer',
+          to: '/lazer'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Quartos',
+          to: '/quartos'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Relatórios',
+          to: '/relatorios'
         }
+
       ],
+
+
       miniVariant: false,
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js'
+
+
     }
   }
 }
 </script>
+
+<style>
+
+@import 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;0,800;1,400;1,600&display=swap';
+
+* {
+  color: #000;
+}
+
+.font {
+  font-family: 'Poppins', sans-serif;
+}
+
+h1 {
+  font-weight: 700;
+  font-size: 3.5rem;
+  margin-left: 0.8rem;
+  color: "#3468FC"!important;
+}
+
+h2 {
+  font-weight: 700;
+  font-size: 2rem;
+}
+
+#header,
+#main {
+  box-shadow: none;
+  background-color: #f4f4f4;
+}
+
+#sidebar {
+  background-color: #fff!important;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+
+/* remove a opacidade cinza nos links */
+.theme--light.v-list-item--active:before, 
+.theme--light.v-list-item--active:hover:before, 
+.theme--light.v-list-item:focus:before {
+    opacity: 0;
+}
+
+
+
+</style>
